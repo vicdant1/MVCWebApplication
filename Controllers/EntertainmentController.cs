4535b04194b86cf34a7310a11b7a1054df4040eb
vicdant1 <dantjv@outlook.com>
2022-03-05 01:14:44 -0300

    estilizando site, criando primeiros controllers e instalando primeiras libs

---
+++ Controllers/EntertainmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCWebApplication.Controllers
+{
+    public class EntertainmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
