be1c2403c62f499bd394c737bc5945ae41eb5ff9
vicdant1 <dantjv@outlook.com>
2022-03-05 16:42:46 -0300

    custom lib

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,12 @@
 ﻿
 $(document).ready(function () {
-    $('.dataTableDisplay').DataTable();
+    $('.dataTableDisplay').DataTable({
+        "language": {
+            "lengthMenu": "_MENU_ records per page",
+            "zeroRecords": "It is kindof empty here :(",
+            "info": "Page _PAGE_ of _PAGES_",
+            "infoEmpty": "There is nothing here,",
+            "infoFiltered": "(searched in _MAX_ total records)"
+        }
+    });
 });
